fee64f8a4111f31c2052acaf883107addfaa6ac2
Nick Schulze <nicks@dwolla.com>
2018-06-22 15:36:55 -0500

    add help stuff

--- index.js
+++ index.js
@@ -1,37 +1,23 @@
-'use strict'
+const got = require('got')
+const indexer = (obj, i) => obj[i]
 
-const fs = require('fs')
-const path = require('path')
-
-module.exports = () => {
-  return (search, env = {}) => {
-    let data = fs.readFileSync('dev-help.json')
-    let json = JSON.parse(data)
-    var keys = []
-    for (var k in json) keys.push(k)
-
-    let searchArray = search.split(' ')
-
-    var hints = []
-    if (keys.includes(searchArray[0])) {
-      var topic = searchArray[0]
-      for (var h in json[topic]) {
-        hints.push({
-          icon: 'fa-terminal',
-          title: h,
-          subtitle: json[topic][h],
-          value: json[topic][h]
-        })
-      }
-    } else {
-      hints.push({
-        icon: 'fa-question-circle',
-        title: search,
-        subtitle: `Nothing found for ${search}.`,
-      })
-    }
-    return new Promise((resolve, reject) => {
-      resolve(hints)
-    })
+module.exports = () => (str, env = {}) => got('api.giphy.com/v1/gifs/search', {
+  json: true,
+  query: {
+    q: str,
+    limit: 5,
+    api_key: 'dc6zaTOxFJmzC'
   }
-}
+}).then(res => res.body.data.map((x, i) => ({
+  icon: 'fa-gift',
+  title: `Giphy Results ${i + 1}`,
+  subtitle: 'Select to copy to the clipboard',
+  value: env.src_main ? env.src_main.split('.').reduce(indexer, x) : x.url,
+  preview: `<style>
+    body {
+      background: url('${x.images.downsized_medium.url}');
+      background-size: cover;
+      background-position: 50% 50%;
+    }
+  </style>`
+})))
